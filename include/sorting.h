5cea60b7ad1fec5cca73bd6778124d7cf1a2532f
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2023-06-04 04:21:05 -0300

    Implementação inicial dos algoritmos de ordenação (merge incorreto)

--- include/sorting.h
+++ include/sorting.h
@@ -1,11 +1,69 @@
 #include "vetor.h"
 
+/// @brief Ordena o vetor para que os itens fiquem na ordem
+/// crescente.
+/// @tparam T O tipo do elemento do vetor
+/// @param vetor_entrada O vetor a ser ordenado
+template <typename T>
+vetor<T> insertionsort(vetor<T>& vetor_entrada) {
+    T aux;
+    int i, j;
+
+    for(i = 1; i < vetor_entrada.size(); ++i) {
+
+        aux = vetor_entrada[i];
+        j = i - 1;
+
+        while(j >= 0 && aux < vetor_entrada[j]) {
+
+            vetor_entrada[j + 1] = vetor_entrada[j];
+            j--;
+
+        }
+        vetor_entrada[j + 1] = aux;
+    }
+
+    return vetor_entrada;
+}
+
 /// @todo
 template <typename T>
-vetor<T> mergesort(vetor<T> vetor_entrada);
+vetor<T> mergesort(vetor<T> vetor_entrada, int esq = 0, int dir = -1) {
+    
+    if (dir == -1)
+        dir = vetor_entrada.size();
+    
+    if (esq < dir) {
+        int meio = (esq + dir) / 2;
+        mergesort(vetor_entrada, esq, meio);
+        mergesort(vetor_entrada, meio + 1, dir);
 
-template <typename T>
-vetor<T> insertionsort(vetor<T> vetor_entrada);
+        vetor<T> temp;
+        int i = esq, j = meio;
+        while (i < meio && j < dir) {
+            if (vetor_entrada[i] <= vetor_entrada[j]) {
+                temp.push_back(vetor_entrada[i]);
+                i++;
+            } else {
+                temp.push_back(vetor_entrada[j]);
+                j++;
+            }
+        }
+
+        for (; i < meio; ++i) {
+            temp.push_back(vetor_entrada[i]);
+        }
+
+        for (; j < dir; ++j) {
+            temp.push_back(vetor_entrada[j]);
+        }
+
+        vetor_entrada = temp;
+
+    }
+
+    return vetor_entrada;
+}
 
 template<typename T>
 vetor<T> linearsort(vetor<T> vetor_entrada);
---
+++ src/sorting.cpp
@@ -0,0 +1,5 @@
+#include "vetor.h"
+#include "sorting.h"
+
+template <typename T>
+vetor<T> insertionsort(vetor<T>& vetor_entrada)
