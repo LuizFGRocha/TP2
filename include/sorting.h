ac455eaf28b360d37480f74f313831092c9b4c76
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2023-06-03 23:15:32 -0300

    Adicionados protótipos da funções de ordenação

---
+++ include/sorting.h
@@ -0,0 +1,11 @@
+#include "vetor.h"
+
+/// @todo
+template <typename T>
+vetor<T> mergesort(vetor<T> vetor_entrada);
+
+template <typename T>
+vetor<T> insertionsort(vetor<T> vetor_entrada);
+
+template<typename T>
+vetor<T> linearsort(vetor<T> vetor_entrada);
