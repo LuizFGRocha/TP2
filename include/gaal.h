e28da3874199ceb5c691e7c67dd179755a12144d
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2023-06-05 20:39:32 -0300

    Adicionados operadores + e -, função comprimento e função sentido

--- include/gaal.h
+++ include/gaal.h
@@ -14,6 +14,16 @@ class ponto {
     /// @return Retorna uma referência para a coordenada y
     int& coordenada_y();
 
+    /// @brief Operador de soma de pontos
+    /// @param rhs O ponto a ser somado com esse
+    /// @return Retorna o ponto resultante da soma de *this com rhs
+    ponto operator+(const ponto& rhs);
+
+    /// @brief Operador de subtração de pontos
+    /// @param rhs O ponto a ser subtraído desse
+    /// @return Retorna o ponto resultante da subtração de *this com rhs
+    ponto operator-(const ponto& rhs);
+
   private:
     int x_m;
     int y_m;
@@ -32,4 +42,15 @@ class reta {
 /// @return Retorna o double correspondente ao ângulo
 double angulo(ponto p, ponto q);
 
-bool nao_vira_para_a_esquerda(ponto p, ponto q);
+/// @brief Retorna o comprimento do segmento de reta que liga p e q
+double comprimento(ponto p, ponto q);
+
+/// @brief Verifica se o menor giro para que a reta que vai de 
+/// p a q tenha a mesma inclinação que a reta que vai de q a r
+/// é no sentido horário.
+/// @param p O primeiro ponto
+/// @param q O segundo ponto
+/// @param r O terceiro ponto
+/// @return Retorna true se o giro for no sentido horário, false 
+/// caso contrário.
+bool gira_sentido_horario(ponto p, ponto q, ponto r);
--- src/gaal.cpp
+++ src/gaal.cpp
@@ -13,6 +13,14 @@ int& ponto::coordenada_y() {
     return y_m;
 }
 
+ponto ponto::operator-(const ponto& rhs) {
+    return ponto(x_m - rhs.x_m, y_m - rhs.y_m);
+}
+
+ponto ponto::operator+(const ponto& rhs) {
+    return ponto(x_m + rhs.x_m, y_m + rhs.y_m);
+}
+
 double angulo(ponto p, ponto q) {
     if (p.coordenada_x() > q.coordenada_x()) { 
         return atan(
@@ -27,8 +35,20 @@ double angulo(ponto p, ponto q) {
     }
 }
 
-bool nao_vira_para_a_esquerda(ponto p, ponto q) {
-    if (angulo(p, q) < M_PI / 2)
+double comprimento(ponto p, ponto q) {
+    ponto vetor_diferenca = q - p;
+    return (sqrt(pow(vetor_diferenca.coordenada_x(), 2) + 
+                pow(vetor_diferenca.coordenada_y(), 2)));
+}
+
+bool gira_sentido_horario(ponto p, ponto q, ponto r) {
+    int valor = (q.coordenada_y() - p.coordenada_y()) * (r.coordenada_x() - q.coordenada_x()) -
+                (q.coordenada_x() - p.coordenada_x()) * (r.coordenada_y() - q.coordenada_y());
+
+    if (valor == 0)
+        return false;
+
+    if (valor > 0)
         return true;
 
     else return false;
