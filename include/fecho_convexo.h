e062eea4084317eb06d84a5d1ea1670367ccef8e
Luiz Fernando <luizfgrocha04@gmail.com>
2023-06-06 02:57:49 -0300

    Troca de pilha para vetor. Algortimo de Graham cumpre os requisitos do livro. Graham funcional.

--- include/fecho_convexo.h
+++ include/fecho_convexo.h
@@ -6,6 +6,7 @@
 #include "gaal.h"
 #include "par.h"
 #include "sorting.h"
+#include "lista.h"
 #include <iostream>
 #include <cmath>
 #include <iostream>
@@ -20,21 +21,20 @@ class fecho_convexo {
 
   public:
 
-    fecho_convexo() : pilha_m() {}
+    fecho_convexo() : vetor_m() {}
 
-    fecho_convexo(pilha<ponto>& pilha) {
-        pilha_m = pilha;
+    fecho_convexo(vetor<ponto>& vetor) {
+        vetor_m = vetor;
     }
 
-    void imprime_e_apaga() {
-        while (!pilha_m.vazia()) {
-            ponto atual = pilha_m.desempilha();
-            std::cout << "(" << atual.coordenada_x() << ", " << atual.coordenada_y() << ")" << std::endl;
-        }
+    void imprime() {
+        for (int i = vetor_m.size() - 1; i >= 0; i--)
+            std::cout << "(" << vetor_m[i].coordenada_x() << ", "
+                      << vetor_m[i].coordenada_y() << ")" << std::endl;
     }
 
   private:
-    pilha<ponto> pilha_m;
+    vetor<ponto> vetor_m;
 };
 
 /// @brief Encontra o ponto com a menor coordenada y e remove esse
@@ -48,9 +48,17 @@ ponto extrai_y_minima(vetor<ponto>& Q) {
     ponto menor = Q[0];
     int posicao_menor = 0;
     for (int i = 1; i < Q.size(); ++i) {
-        if (Q[i].coordenada_y() < menor.coordenada_y())
+
+        if (Q[i].coordenada_y() < menor.coordenada_y()) {
             menor = Q[i];
             posicao_menor = i;
+
+        } else if (Q[i].coordenada_y() == menor.coordenada_y()) {
+
+            if (Q[i].coordenada_x() < menor.coordenada_x())
+                menor = Q[i];
+                posicao_menor = i;
+        }
     }
 
     Q.remove(posicao_menor);
@@ -60,13 +68,13 @@ ponto extrai_y_minima(vetor<ponto>& Q) {
 
 fecho_convexo varredura_de_graham(vetor<ponto> Q, int metodo_de_ordenacao) {
 
-    // Passo 1 do pseudocódigo
+    // Passo 1
     ponto p0 = extrai_y_minima(Q); // p0 é identificado e removido de Q
 
-    // Passo 2 do pseudocódigo
+    // Passo 2
     vetor<par<ponto, double>> vetor_ponto_angulo; // Vetor de pares ponto-ângulo
     for (int i = 0; i < Q.size(); ++i)
-        vetor_ponto_angulo.push_back(par<ponto, double>(Q[i], angulo(Q[i], p0)));
+        vetor_ponto_angulo.push_back(par<ponto, double>(Q[i], angulo(p0, Q[i])));
 
     switch (metodo_de_ordenacao) {
         case k_mergesort:
@@ -82,44 +90,46 @@ fecho_convexo varredura_de_graham(vetor<ponto> Q, int metodo_de_ordenacao) {
             throw excecao_metodo_de_ordenacao_invaido{};
     }
 
-    std::cout << "(" << p0.coordenada_x() << ", " << p0.coordenada_y() << ")" << std::endl;
-    for (int i = 0; i < vetor_ponto_angulo.size(); ++i)
-        std::cout << "(" << vetor_ponto_angulo[i].primeiro().coordenada_x() << ", " << vetor_ponto_angulo[i].primeiro().coordenada_y() << ") " << vetor_ponto_angulo[i].segundo() << std::endl;
+    // Remove empates
+    for (int i = 0; i < vetor_ponto_angulo.size() - 1; ++i)
+        if (vetor_ponto_angulo[i].segundo() == vetor_ponto_angulo[i + 1].segundo())
+            comprimento(p0, vetor_ponto_angulo[i].primeiro()) > comprimento(p0, vetor_ponto_angulo[i + 1].primeiro()) ? vetor_ponto_angulo.remove(i + 1) : vetor_ponto_angulo.remove(i);
 
-    // Passo 3 do pseudocódigo
-    pilha<ponto> S;
+    // Passo 3
+    vetor<ponto> S;
 
-    // Passo 4 do pseudocódigo
-    S.empilha(p0);
-    std::cout << "Primeiro ponto adicionado: ";
-    p0.imprime_ponto();
+    // Passo 4
+    S.push_back(p0);
 
-    // Passo 5 do pseudocódigo
-    S.empilha(vetor_ponto_angulo[0].primeiro());
+    // Passo 5
+    S.push_back(vetor_ponto_angulo[0].primeiro());
 
-    // Passo 6 do pseudocódigo
+    // Passo 6
     if (vetor_ponto_angulo.size() >= 3)
-        S.empilha(vetor_ponto_angulo[1].primeiro());
+        S.push_back(vetor_ponto_angulo[1].primeiro());
 
-    // Passo 7 do pseudocódigo
+    // Passo 7
     for (int i = 2; i < vetor_ponto_angulo.size(); ++i) {
 
-        // Passo 8 do pseudocódigo
-        while (gira_sentido_horario(S.espia_segundo(), S.espia_segundo(), vetor_ponto_angulo[i].primeiro())) {
+        // Passo 8
+        while (gira_sentido_horario(S[S.size() - 1], S[S.size() - 2], vetor_ponto_angulo[i].primeiro())) {
             
             // Passo 9 do psedocódigo
-            ponto p = S.desempilha();
-            std::cout << "Ponto removido: ";
-            p.imprime_ponto();
+            ponto p = S.pop_back();
+
         }
 
-        // Passo 10 do pseudocódigo
-        S.empilha(vetor_ponto_angulo[i].primeiro());
-        std::cout << "Ponto adicionado: ";
-        vetor_ponto_angulo[i].primeiro().imprime_ponto();
+        // Passo 10
+        S.push_back(vetor_ponto_angulo[i].primeiro());
+        
     }
 
-    // Passo 11 do pseudocódigo
+    // Remove pontos colineares
+    for (int i = 0; i < S.size(); ++i)
+        if (sao_colineares(S[i], S[(i+1) % S.size()], S[(i+2) % S.size()]))
+            S.remove((i + 1) % S.size());
+
+    // Passo 11
     return S;
 }
 
