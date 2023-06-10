48877c0d70830a1fa852fd1b094120083a7ba75d
Luiz Fernando <luizfgrocha04@gmail.com>
2023-06-10 18:29:40 -0300

    Adicionada opção de salvar vetor aleatório. Dada permissão de utilização de números maiores nos pontos do vetor aleatório.

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ struct args {
     bool f_experimento;
     int tamanho_experimento;
     string arquivo_saida_experimento; 
+    bool salvar;
 };
 
 void parse_args(args& argumentos, int argc, char** argv) {
@@ -28,8 +29,9 @@ void parse_args(args& argumentos, int argc, char** argv) {
 
     argumentos.f_experimento = false;
     argumentos.tamanho_experimento = 0;
+    argumentos.salvar = false;
 
-    while ((c = getopt(argc, argv, "p:xt:o:")) != EOF) {
+    while ((c = getopt(argc, argv, "p:xt:o:i")) != EOF) {
         switch (c) {
             case 'p':
                 argumentos.arquivo_entrada = optarg;
@@ -46,6 +48,8 @@ void parse_args(args& argumentos, int argc, char** argv) {
                 argumentos.arquivo_saida_experimento = optarg;
                 f_sai = true;
                 break;
+            case 'i':
+                argumentos.salvar = true;
             case '?':
                 cerr << "Erro ao ler os argumentos." << endl;
                 throw excecao_erro_ao_ler_argumentos{};
@@ -70,7 +74,7 @@ void gera_vetor_aleatorio(vetor<ponto>& vetor_entrada, int tamanho) {
     
     vetor_entrada.clean();
     for (int i = 0; i < tamanho; ++i) {
-        vetor_entrada.push_back(ponto(rand() % 1000000, rand() % 1000000));
+        vetor_entrada.push_back(ponto(rand(), rand()));
     }
 } 
 
@@ -102,36 +106,42 @@ int main(int argc, char** argv) {
 
     vetor<ponto> vetor_de_pontos;
 
-    if (argumentos.f_experimento) {
+    if (argumentos.f_experimento || argumentos.tamanho_experimento > 0) {
         if (argumentos.tamanho_experimento > 0) {
             vetor_de_pontos.set_capacity(argumentos.tamanho_experimento);
             gera_vetor_aleatorio(vetor_de_pontos, argumentos.tamanho_experimento);
+
+            if (argumentos.salvar) {
+                ofstream vetor_aleatorio("./vetor_utilizado.txt");
+                for (int i = 0; i < vetor_de_pontos.size(); ++i) 
+                    vetor_aleatorio << vetor_de_pontos[i].coordenada_x() << " " << vetor_de_pontos[i].coordenada_y() << std::endl; 
+            }
         }
         else 
             le_de_arquivo_para_vetor(argumentos.arquivo_entrada, vetor_de_pontos);
     } else 
         le_de_arquivo_para_vetor(argumentos.arquivo_entrada, vetor_de_pontos);
 
-    fecho_convexo resultado;
+    fecho_convexo resultado_jarvis, resultado_graham_merge, resultado_graham_insertion, resultado_graham_bucket;
 
     marca_inicio();
-    marcha_de_jarvis(vetor_de_pontos);
+    resultado_jarvis = marcha_de_jarvis(vetor_de_pontos);
     double tempo_jarvis = fim_da_medida();
 
     marca_inicio();
-    resultado = varredura_de_graham(vetor_de_pontos, k_insertionsort);
+    resultado_graham_insertion = varredura_de_graham(vetor_de_pontos, k_insertionsort);
     double tempo_insertion = fim_da_medida();
 
     marca_inicio();
-    varredura_de_graham(vetor_de_pontos, k_mergesort);
+    resultado_graham_merge = varredura_de_graham(vetor_de_pontos, k_mergesort);
     double tempo_merge = fim_da_medida();
 
     marca_inicio();
-    varredura_de_graham(vetor_de_pontos, k_bucketsort);
+    resultado_graham_bucket = varredura_de_graham(vetor_de_pontos, k_bucketsort);
     double tempo_bucket = fim_da_medida();
 
     if (!argumentos.f_experimento) {
-        resultado.imprime();
+        resultado_graham_bucket.imprime();
 
         cout << setprecision(3) << fixed;
 
